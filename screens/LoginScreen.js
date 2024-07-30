ee5cae3d2f85b2cedfb582ad170b020584162531
loshan <it21803970@my.sliit.lk>
2024-07-30 17:48:10 +0530

    login and signup page with reanimated

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,29 +1,73 @@
-import { Image, StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import Animated, {FadeInDown, FadeInUp } from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
+    const navigation = useNavigation()
   return (
-    <View className = "bg-white h-full w-full">
-        <StatusBar barStyle={'light-content'}/>
-        <Image className="h-full w-full absolute" source={ require("../Assets/images/background.png")}/>
-        <View className="flex-row justify-around w-full absolute">
-            <Image className="h-[225] w-[90]" source={require("../Assets/images/light.png")}/>
-            <Image className="h-[160] w-[65]" source={require("../Assets/images/light.png")}/>
-
-            
+    <View className="bg-white h-full w-full">
+      <StatusBar barStyle={"light-content"} />
+      <Image
+        className="h-full w-full absolute"
+        source={require("../Assets/images/background.png")}
+      />
+      <View className="flex-row justify-around w-full absolute">
+        <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify().damping(3)}
+          className="h-[225] w-[90]"
+          source={require("../Assets/images/light.png")}
+        />
+        <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify().damping(3)}
+          className="h-[160] w-[65]"
+          source={require("../Assets/images/light.png")}
+        />
+      </View>
+      <View className="h-full w-full flex justify-around pt-40 pb-10">
+        <View className="flex items-center">
+          <Animated.Text entering={FadeInUp.duration(1000).springify()}
+          className="text-white font-bold tracking-wider text-5xl">
+            Login
+          </Animated.Text>
         </View>
-        <View className="h-full w-full flex justify-around pt-40 pb-10">
-                <View className="flex items-center">
-                    <Text className="text-black font-bold tracking-wider text-5xl">Login</Text>
-
-                </View>
-                <View className="flex items-center mx-4 space-y-4"></View>
-            
+        <View className="flex items-center mx-4 space-y-4">
+          <Animated.View entering={FadeInDown.duration(1000).springify()}
+          className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder="Email" placeholderTextColor={"gray"} />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+            <TextInput
+              placeholder="password"
+              placeholderTextColor={"gray"}
+              secureTextEntry
+            />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="w-full">
+            <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+              <Text className="text-xl font-bold text-white text-center">
+                Login
+              </Text>
+            </TouchableOpacity>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="flex-row justify-center">
+            <Text>Don't have an account? </Text>
+            <TouchableOpacity onPress={()=>navigation.push('SignUp')}>
+              <Text className="text-sky-600">SignUp</Text>
+            </TouchableOpacity>
+          </Animated.View>
         </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
